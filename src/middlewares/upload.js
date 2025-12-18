b3394dbb06ed324cb9b1910ca7bb6aa2c4fe0297
azuretigerstudios <azuretigerstudios@gmail.com>
2025-12-18 22:13:05 +0300

    upload dosyası oluşturuldu

---
+++ src/middlewares/upload.js
@@ -0,0 +1,17 @@
+// src/middlewares/upload.js
+import multer from 'multer';
+import { CloudinaryStorage } from '../cloudinary/index.cloudinary.js';
+import cloudinary from '../cloudinary/index.cloudinary.js';
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: (req, file) => {
+    return {
+        folder: 'contacts_photos', // Yüklenen dosyaların saklanacağı klasör
+        allowed_formats: ['jpg', 'png', 'webp'],
+        public_id: file.originalname.split('.')[0],
+    };
+  },
+});
+
+export const upload = multer({ storage });
